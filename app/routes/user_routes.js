8083e81249b11fee698ac821b7b1e25276f9ca94
Hitesh Jain <jainhitesh90@gmail.com>
2017-07-15 23:18:28 +0530

    user validation for login and signup

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,22 +1,31 @@
 module.exports = function (app, db) {
     var ObjectID = require('mongodb').ObjectID;
 
-    /* SIGNUP or LOGIN */
-    app.post('/user/login', (req, res) => {
+    /* SIGNUP / LOGIN */
+    app.post('/user/signup', (req, res) => {
         if (req.body.email == null || req.body.email == '') {
             res.send(errorResponse('Email Id missing')); 
+        } else if (req.body.id == null || req.body.id == '') {
+            res.send(errorResponse('Social login id missing')); 
         } else {
-            const user = { email: req.body.email };
-            var cursor = db.collection('users').find(user);
+            var cursor = db.collection('users').find({ 
+                email : req.body.email, socialId : req.body.id
+            });
             cursor.toArray(function (err, docs) {
-                if (docs.length > 0) {
+                if (err) {
+                    res.send(errorResponse(err.errmsg));
+                } else if (docs.length > 0){
                     res.send(successResponse('Welcome back!!' , docs[0]))
                 } else {
-                    var token = getToken(req.body.email)
-                    const userObject = { email: req.body.email, token: token };
+                    /* create profile */
+                    var authToken = getToken(req.body.email)
+                    const userObject = { email: req.body.email, socialId : req.body.id, authToken : authToken };
                     db.collection('users').insert(userObject, (err, result) => {
                         if (err) {
-                            res.send(errorResponse(err.errmsg));
+                            if (String(err.errmsg).includes('E11000')) //duplicate email id
+                                res.send(errorResponse("Please login using proper social media account"));
+                            else
+                                res.send(errorResponse(err.errmsg))
                         } else {
                             res.send(successResponse('Profile created!!' , result.ops[0]))
                         }
@@ -39,14 +48,14 @@ module.exports = function (app, db) {
     });
 };
 
-/* get AWT token */
+/* get AWT authToken */
 function getToken(email) {
     const db = require('../../config/db');
     var jwt = require('jsonwebtoken');
-    var token = jwt.sign({ email: email }, db.secretKey, {
+    var authToken = jwt.sign({ email: email }, db.secretKey, {
         expiresIn: 365 * 24 * 60  // expires in 1 year
     });
-    return token;
+    return authToken;
 }
 
 function errorResponse(errorMsg) {
