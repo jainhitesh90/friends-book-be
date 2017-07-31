53adb3e28f160f871d9258af8023058f65d357f1
Hitesh Jain <jainhitesh90@gmail.com>
2017-08-01 01:57:02 +0530

    updated code to v2

--- app/routes/feeds_routes.js
+++ app/routes/feeds_routes.js
@@ -15,45 +15,93 @@ module.exports = function (app, db) {
         var s3Bucket = new AWS.S3({ params: { Bucket: configFile.s3BucketName } })
         var fs = require('fs');
         var file = req.file;
-        if (req.body.contentDescription == null || req.body.contentDescription == '') {
-            res.send(utils.errorResponse('Describe your feed'))
-        } else if (file == null || file.path == null) {
-            const feed = { userId: userId, contentDescription: req.body.contentDescription, createdAt: Date.now() };
-            db.collection('feeds').insert(feed, (err, result) => {
-                if (err) {
-                    res.send(utils.errorResponse(err.errmsg));
-                } else {
-                    res.send(utils.successResponse("Feed added successfully", result.ops[0]))
-                }
-            });
-        } else {
-            fs.readFile(file.path, function (err, data) {
-                if (err) throw err; // Something went wrong!
-                s3Bucket.createBucket(function () {
-                    var params = null
-                    if (req.body.contentType == 'image')
-                        params = { Key: file.filename, Body: data, ContentType: 'image/png', ACL: 'public-read' };
-                    else if (req.body.contentType == 'video')
-                        params = { Key: file.filename, Body: data, ContentType: 'video', ACL: 'public-read' };
-                    s3Bucket.upload(params, function (err, data) {
-                        fs.unlink(file.path, function (err) {
-                            if (err) { console.error(err); }
-                        });
-                        if (err) {
-                            res.send(utils.errorResponse('Something went wrong!!'))
-                        } else {
-                            const feed = { userId: userId, contentType: req.body.contentType, contentDescription: req.body.contentDescription, contentUrl: data['Location'], createdAt: Date.now() };
-                            db.collection('feeds').insert(feed, (err, result) => {
-                                if (err) {
-                                    res.send(utils.errorResponse(err.errmsg));
-                                } else {
-                                    res.send(utils.successResponse("Feed added successfully", result.ops[0]))
-                                }
+        if (req.body.feedType == 'post') {
+            if (req.body.description == null || req.body.description == '') {
+                res.send(utils.errorResponse('Describe your feed'))
+            } else if (file == null || file.path == null) {
+                const feed = { feedType: req.body.feedType, userId: userId, description: req.body.description, createdAt: Date.now() };
+                db.collection('feeds').insert(feed, (err, result) => {
+                    if (err) {
+                        res.send(utils.errorResponse(err.errmsg));
+                    } else {
+                        res.send(utils.successResponse("Feed added successfully", result.ops[0]))
+                    }
+                });
+            } else {
+                fs.readFile(file.path, function (err, data) {
+                    if (err) throw err; // Something went wrong!
+                    s3Bucket.createBucket(function () {
+                        var params = null
+                        if (req.body.contentType == 'image')
+                            params = { Key: file.filename, Body: data, ContentType: 'image/png', ACL: 'public-read' };
+                        else if (req.body.contentType == 'video')
+                            params = { Key: file.filename, Body: data, ContentType: 'video', ACL: 'public-read' };
+                        s3Bucket.upload(params, function (err, data) {
+                            fs.unlink(file.path, function (err) {
+                                if (err) { console.error(err); }
                             });
-                        }
+                            if (err) {
+                                res.send(utils.errorResponse('Something went wrong!!'))
+                            } else {
+                                const feed = { feedType: req.body.feedType, userId: userId, contentType: req.body.contentType, description: req.body.description, contentUrl: data['Location'], createdAt: Date.now() };
+                                db.collection('feeds').insert(feed, (err, result) => {
+                                    if (err) {
+                                        res.send(utils.errorResponse(err.errmsg));
+                                    } else {
+                                        res.send(utils.successResponse("Feed added successfully", result.ops[0]))
+                                    }
+                                });
+                            }
+                        });
                     });
                 });
-            });
+            }
+        } else if (req.body.feedType == 'event') {
+            if (req.body.title == null || req.body.title == '') {
+                res.send(utils.errorResponse('Title missing'));
+            } else if (req.body.description == null || req.body.description == '') {
+                res.send(utils.errorResponse('Description missing'));
+            } else if (req.body.venue == null || req.body.venue == '') {
+                res.send(utils.errorResponse('Venue missing'));
+            } else if (req.body.price == null || req.body.price == '') {
+                res.send(utils.errorResponse('Price missing'));
+            } else if (req.body.time == null || req.body.time == '') {
+                res.send(utils.errorResponse('Time missing'));
+            } else if (req.body.url == null || req.body.url == '') {
+                res.send(utils.errorResponse('Url missing'));
+            } else if (req.body.image == null || req.body.image == '') {
+                res.send(utils.errorResponse('Image URL missing'));
+            } else {
+                const event = { feedType: req.body.feedType, userId: userId, title: req.body.title, description: req.body.description, venue: req.body.venue, price: req.body.price, time: req.body.time, url: req.body.url, image: req.body.image, createdAt: Date.now() };
+                db.collection('feeds').insert(event, (err, result) => {
+                    if (err) {
+                        res.send(utils.errorResponse(err.errmsg));
+                    } else {
+                        res.send(utils.successResponse(null, result.ops[0]))
+                    }
+                });
+            }
+        } else if (req.body.feedType == 'blog') {
+            if (req.body.title == null || req.body.title == '') {
+                res.send(utils.errorResponse('Title missing'));
+            } else if (req.body.description == null || req.body.description == '') {
+                res.send(utils.errorResponse('Description missing'));
+            } else if (req.body.fullUrl == null || req.body.fullUrl == '') {
+                res.send(utils.errorResponse('Full URL missing'));
+            } else if (req.body.image == null || req.body.image == '') {
+                res.send(utils.errorResponse('Image URL missing'));
+            } else {
+                const blog = { feedType: req.body.feedType, userId: userId, title: req.body.title, description: req.body.description, fullUrl: req.body.fullUrl, image: req.body.image, createdAt: Date.now() };
+                db.collection('feeds').insert(blog, (err, result) => {
+                    if (err) {
+                        res.send(utils.errorResponse(err.errmsg));
+                    } else {
+                        res.send(utils.successResponse(null, result.ops[0]))
+                    }
+                });
+            }
+        } else {
+            res.send(utils.errorResponse("Feed type invalid"));
         }
     });
 
@@ -61,13 +109,48 @@ module.exports = function (app, db) {
     app.put('/feed/update/:id', utils.isUserAuthenticated, (req, res) => {
         if (req.params.id == null) {
             res.send(utils.errorResponse('Feed id missing'));
-        } else if (req.body.contentDescription == null || req.body.contentDescription == '') {
-            res.send(utils.errorResponse('Please add some description'))
         } else {
             const id = req.params.id;
             const details = { '_id': new ObjectID(id) };
-            const feed = { $set: { contentDescription: req.body.contentDescription, updatedAt: Date.now() } };
-            db.collection('feeds').update(details, feed, (err, result) => {
+            var updatedDoc = {}
+            if (req.body.feedType == 'post') {
+                if (req.body.description == null || req.body.description == '') {
+                    res.send(utils.errorResponse('Please add some description'))
+                } else {
+                    updatedDoc = { $set: { description: req.body.description, updatedAt: Date.now() } };
+                }
+            } else if (req.body.feedType == 'event') {
+                if (req.body.title == null || req.body.title == '') {
+                    res.send(utils.errorResponse('Title missing'));
+                } else if (req.body.description == null || req.body.description == '') {
+                    res.send(utils.errorResponse('Description missing'));
+                } else if (req.body.venue == null || req.body.venue == '') {
+                    res.send(utils.errorResponse('Venue missing'));
+                } else if (req.body.price == null || req.body.price == '') {
+                    res.send(utils.errorResponse('Price missing'));
+                } else if (req.body.time == null || req.body.time == '') {
+                    res.send(utils.errorResponse('Time missing'));
+                } else if (req.body.url == null || req.body.url == '') {
+                    res.send(utils.errorResponse('Url missing'));
+                } else if (req.body.image == null || req.body.image == '') {
+                    res.send(utils.errorResponse('Image URL missing'));
+                } else {
+                    updatedDoc = { $set: { title: req.body.title, description: req.body.description, venue: req.body.venue, price: req.body.price, time: req.body.time, url: req.body.url, image: req.body.image, updatedAt: Date.now() } };
+                }
+            } else if (req.body.feedType == 'blog') {
+                if (req.body.title == null || req.body.title == '') {
+                    res.send(utils.errorResponse('Title missing'));
+                } else if (req.body.description == null || req.body.description == '') {
+                    res.send(utils.errorResponse('Description missing'));
+                } else if (req.body.fullUrl == null || req.body.fullUrl == '') {
+                    res.send(utils.errorResponse('Full URL missing'));
+                } else if (req.body.image == null || req.body.image == '') {
+                    res.send(utils.errorResponse('Image URL missing'));
+                } else {
+                    updatedDoc = { $set: { title: req.body.title, description: req.body.description, fullUrl: req.body.fullUrl, image: req.body.image, updatedAt: Date.now() } };
+                }
+            }
+            db.collection('feeds').update(details, updatedDoc, (err, result) => {
                 if (err) {
                     res.send(utils.errorResponse(err.errmsg));
                 } else {
@@ -101,7 +184,9 @@ module.exports = function (app, db) {
         } else {
             var cursor = db.collection('feeds').find({
                 $or: [
-                    { contentDescription: { $regex: ".*" + req.params.keyword + ".*", '$options': 'i' } }
+                    { description: { $regex: ".*" + req.params.keyword + ".*", '$options': 'i' } },
+                    { title: { $regex: ".*" + req.params.keyword + ".*", '$options': 'i' } },
+                    { description: { $regex: ".*" + req.params.keyword + ".*", '$options': 'i' } }
                 ]
             });
             cursor.toArray(function (err, docs) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,8 @@
-const blogRoutes = require('./blog_routes');
-const eventRoutes = require('./event_routes');
 const userRoutes = require('./user_routes');
 const adminRoutes = require('./admin_routes');
 const feedsRoutes = require('./feeds_routes');
 
 module.exports = function(app, db) {
-  blogRoutes(app, db);
-  eventRoutes(app, db);
   userRoutes(app, db);
   adminRoutes(app, db);
   feedsRoutes(app, db);
