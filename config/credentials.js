cba0b01b5ff0fbf8b1032b8bfb4d4574148c83f3
Hitesh Jain <jainhitesh90@gmail.com>
2017-08-19 20:47:17 +0530

    remved config

--- config/credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  url : 'mongodb://jainhitesh90:jainhitesh90@ds159112.mlab.com:59112/testing-db',
-  secretKey : 'mySecretKey',
-  s3BucketName : 'music-feeds-images',
-  fcmServerKey : 'AIzaSyAimrKZDX2EMmDYt9-Q9chxB5Q7czij-VM'
-};
