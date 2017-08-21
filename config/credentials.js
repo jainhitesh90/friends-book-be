025b0616986dbf4f86e0e4a5969cdb8edf637539
Hitesh Jain <jainhitesh90@gmail.com>
2017-08-21 18:18:38 +0530

    Added dummy keys

---
+++ config/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+    url : 'mongodb://jainhitesh90:jainhitesh90@ds159112.mlab.com:59112/testing-db',
+    secretKey : 'dummy',
+    s3BucketName : 'dummy',
+    fcmServerKey : 'dummy'
+};
